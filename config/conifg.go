41a1e91c7e926177df5f381606c20bff9afcba75
rasimin <rasimin@praisindo.co.id>
2024-07-30 10:10:55 +0700

    konkfigurasi dengan docker

--- config/conifg.go
+++ config/conifg.go
@@ -13,7 +13,7 @@ type Config struct {
 
 func LoadConfig() *Config {
 	return &Config{
-		DatabaseDSN: getEnv("DATABASE_DSN", "postgresql://postgres:admin@localhost:5432/tugas_shorturl"),
+		DatabaseDSN: getEnv("DATABASE_DSN", "postgresql://postgres:admin@127.0.0.1:5432/tugas_shorturl"),
 		RedisAddr:   getEnv("REDIS_ADDR", "127.0.0.1:6379"),
 		GRPCPort:    getEnv("GRPC_PORT", ":50051"),
 		HTTPPort:    getEnv("HTTP_PORT", ":8080"),
